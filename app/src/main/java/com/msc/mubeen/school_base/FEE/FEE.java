a5fcb9e7d034b613aef4dcaaef490818647339b2
Moon <moonmajeed945@gmail.com>
2019-05-27 04:40:59 +0500

    Source Code

---
+++ app/src/main/java/com/msc/mubeen/school_base/FEE/FEE.java
@@ -0,0 +1,15 @@
+package com.msc.mubeen.school_base.FEE;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.msc.mubeen.school_base.R;
+
+public class FEE extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fee);
+    }
+}
